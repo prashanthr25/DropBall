a21260a950afad8a5f57ee555fefa94025303960
prashanthr25 <prashanthr25@gmail.com>
2014-04-14 23:09:46 +0530

    First Commit

---
+++ EBMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  EBMainViewController.h
+//  EggBucket
+//
+//  Created by Prashanth Rajagopalan on 4/14/14.
+//  Copyright (c) 2014 TechiWorld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EBMainViewController : UIViewController
+
+@end
---
+++ EBMainViewController.m
@@ -0,0 +1,47 @@
+//
+//  EBMainViewController.m
+//  EggBucket
+//
+//  Created by Prashanth Rajagopalan on 4/14/14.
+//  Copyright (c) 2014 TechiWorld. All rights reserved.
+//
+
+#import "EBMainViewController.h"
+
+@interface EBMainViewController ()
+
+@end
+
+@implementation EBMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [UIView animateWithDuration:0.3 delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+        
+        
+    } completion:^(BOOL finished) {
+        
+    
+    }];
+    
+
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
